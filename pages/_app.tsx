2389d5bb37b8c975c8a509c7ec131ac89db3c8b0
Suzu Tomo <kamekyame@outlook.com>
2021-09-26 02:09:13 +0900

    Add layout file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,60 @@
-import '../styles/globals.css'
-import Head from 'next/head'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import type { AppProps } from "next/app";
 
-import Header from "../components/header"
-import Footer from "../components/footer"
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, styled } from "@mui/material/styles";
+import { CacheProvider, EmotionCache } from "@emotion/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (<div>
-    <Head>
-    <meta charSet="UTF-8" />
-    <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-    <link rel="icon" type="image/png" href="/img/kakomimasu-icon.png" />
-    <link rel="apple-touch-icon" href="/img/kakomimasu-icon.png" />
+import Footer from "../components/footer";
+import Heater from "../components/header";
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+import theme from "../src/theme";
+import createEmotionCache from "../src/createEmotionCache";
 
-    <title>囲みマス</title>
+const clientSideEmotionCache = createEmotionCache();
 
-    </Head>
-    {/*<Header />*/}
-    <Component {...pageProps} />
-  <Footer />
-  </div>)
+const Body = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  height: "100%",
+});
+
+const Main = styled("div")({
+  flexGrow: 1,
+  width: "90%",
+  maxWidth: "1000px",
+  padding: "3em 0",
+  margin: "0 auto",
+});
+
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: AppProps & {
+  emotionCache?: EmotionCache;
+}) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>囲みマス</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+        <Body>
+          {<Heater />}
+          <Main>
+            <Component {...pageProps} />
+          </Main>
+          <Footer />
+        </Body>
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
-export default MyApp
+export default MyApp;
