bc64fc321d8b33a52f32643d1ae550e939b1d720
ninja03 <69106571+ninja03@users.noreply.github.com>
2021-10-03 21:54:58 +0900

    Update apiClient.ts

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -1,6 +1,6 @@
 import ApiClient from "client-js";
 export * from "client-js";
 
-export const host = "localhost:8880";
+export const host = "practice.kakomimasu.website";
 
 export const apiClient = new ApiClient("http://" + host);
