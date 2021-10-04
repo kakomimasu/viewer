dadf947150f67e79e60a2388bef67443d8d1d3e2
ninja03 <69106571+ninja03@users.noreply.github.com>
2021-10-04 22:43:13 +0900

    修正

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -3,5 +3,10 @@ export * from "client-js";
 
 export const host = process.env.NEXT_PUBLIC_HOST;
 
-export const apiClient = new ApiClient(window.location.protocol + "//" + host);
-
+let protocol: string
+if (host === "localhost" || host?.startsWith("localhost:")) {
+  protocol = "http:";
+} else {
+  protocol = "https:";
+}
+export const apiClient = new ApiClient(protocol + "//" + host);
