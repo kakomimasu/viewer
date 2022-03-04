549ba98dba1f206088472d4a50a30f22b0c8996f
Suzu Tomo <kamekyame@outlook.com>
2022-03-05 01:54:41 +0900

    fix: コンテンツの余白を_documentや_appで指定しないように変更

--- components/content.tsx
+++ components/content.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import Head from "next/head";
+import { Box } from "@mui/material";
 
 type Props = {
-  children?: React.ReactNode;
   title: string;
 };
 
 const Content: React.FC<Props> = (props) => {
   return (
-    <>
+    <Box
+      sx={{
+        width: "90%",
+        maxWidth: "1000px",
+        padding: "3em 0",
+        margin: "0 auto",
+      }}
+    >
       <Head>
         <title>{props.title} - 囲みマス</title>
       </Head>
       <h1>{props.title}</h1>
       {props.children}
-    </>
+    </Box>
   );
 };
 
