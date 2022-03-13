c112ef27bd9553095677c65e656c350d5fdc50d1
Suzu Tomo <kamekyame@outlook.com>
2022-03-13 15:10:58 +0900

    fix: 即時実行関数を一部削除

--- components/gameBoard.tsx
+++ components/gameBoard.tsx
@@ -61,10 +61,13 @@ const AgentDetail = styled("div")({
   },
 });
 
-export default function GameBoard({ game, users }: Props) {
+export default function GameBoard({
+  game: { board, tiled, players, log },
+  users,
+}: Props) {
   const isAgent = (x: number, y: number) => {
-    if (game.players) {
-      const agent = game.players
+    if (players) {
+      const agent = players
         .map((e, i) =>
           e.agents.map((e_, j) => {
             return { agent: e_, player: i, n: j };
@@ -77,7 +80,6 @@ export default function GameBoard({ game, users }: Props) {
   };
   const agentHistory = (agent: ReturnType<typeof isAgent>) => {
     if (!agent) return [];
-    const log = game.log;
     if (!log) return [];
     const pid = agent.player,
       aid = agent.n;
@@ -107,8 +109,8 @@ export default function GameBoard({ game, users }: Props) {
   };
 
   const getAgentTransform = (x: number, y: number) => {
-    const w = game.board.width;
-    const h = game.board.height;
+    const w = board.width;
+    const h = board.height;
     const transX = x < w / 2 ? "0%" : "-100%";
     const transY = y < h / 2 ? "0%" : "-100%";
     return `translate(${transX},${transY})`;
@@ -118,13 +120,12 @@ export default function GameBoard({ game, users }: Props) {
 
   const scale = useMemo(() => {
     if (!width || !height) return 1;
-    const idealWidth = (game.board.width + 2) * 50;
-    const idealHeight = (game.board.height + 2) * 50;
+    const idealWidth = (board.width + 2) * 50;
+    const idealHeight = (board.height + 2) * 50;
     const scaleX = width / idealWidth;
     const scaleY = height / idealHeight;
-    // console.log(scaleX, scaleY);
     return Math.min(scaleX, scaleY);
-  }, [width, height, game.board]);
+  }, [width, height, board]);
 
   return (
     <Box
@@ -138,215 +139,200 @@ export default function GameBoard({ game, users }: Props) {
       }}
       ref={ref}
     >
-      {(() => {
-        const board = game.board;
-        const tiled = game.tiled;
-        if (!tiled) return;
+      <Box
+        id="field"
+        sx={{
+          userSelect: "none",
+          display: "grid",
+          gridAutoColumns: "50px",
+          gridAutoRows: "50px",
+          gap: "1px",
+          lineHeight: "1",
+          fontSize: `15px`,
+          transform: `scale(${scale})`,
+        }}
+      >
+        {[1, board.height + 2].map((y) => {
+          return new Array(board.width).fill(0).map((_, i) => {
+            const x = i + 1;
+            return (
+              <Box
+                key={`index-${x}-${y}`}
+                sx={{
+                  gridColumn: x + 1,
+                  gridRow: y,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  fontWeight: "bold",
+                }}
+              >
+                {x}
+              </Box>
+            );
+          });
+        })}
+        {[1, board.width + 2].map((x) => {
+          return new Array(board.width).fill(0).map((_, i) => {
+            const y = i + 1;
+            return (
+              <Box
+                key={`index-${x}-${y}`}
+                sx={{
+                  gridColumn: x,
+                  gridRow: y + 1,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  fontWeight: "bold",
+                }}
+              >
+                {y}
+              </Box>
+            );
+          });
+        })}
+        {(() => {
+          return board.points.map((point, i) => {
+            const tile: NonNullable<typeof tiled>[number] = (tiled &&
+              tiled[i]) || { type: 0, player: null };
+            const y = Math.floor(i / board.width);
+            const x = i % board.width;
+            // const point = board.points[i];
+            const isAbs = point < 0 && tile.player !== null && tile.type === 0;
+            const agent = (() => {
+              if (players) {
+                const agent = players
+                  .map((e, i) =>
+                    e.agents.map((e_, j) => {
+                      return { agent: e_, player: i, n: j };
+                    })
+                  )
+                  .flat()
+                  .find((e) => e.agent.x === x && e.agent.y === y);
+                return agent;
+              } else return undefined;
+            })();
+            const bgColor = () => {
+              if (tile.player !== null) {
+                //console.log(tile);
+                //console.log("tile player", tile.player);
+                return datas[tile.player].colors[tile.type];
+              } else if (point < 0) {
+                const l = 100 - (Math.abs(point) * 50) / 16;
+                return `hsl(0,0%,${l}%)`;
+              } else if (point > 0) {
+                const l = 100 - (Math.abs(point) * 50) / 16;
+                return `hsl(60,100%,${l}%)`;
+              }
+            };
+            const isConflict = log
+              ? (() => {
+                  const lastActLog = log.at(-1)?.players.flatMap((e) => {
+                    if (e.actions) return [...e.actions];
+                    else return [];
+                  });
+                  const isConflict = lastActLog?.some(
+                    (a) => a.res > 0 && a.res < 3 && a.x === x && a.y === y
+                  );
+                  return isConflict;
+                })()
+              : false;
 
-        return (
-          <Box
-            id="field"
-            sx={{
-              userSelect: "none",
-              display: "grid",
-              gridTemplateColumns: `min-content repeat(${board.width}, 50px) min-content`,
-              gridTemplateRows: `min-content repeat(${board.height}, 1fr) min-content`,
-              gap: "1px",
-              lineHeight: "1",
-              fontSize: `clamp(10px, calc(25vw/${board.width}), 15px)`,
-              transform: `scale(${scale})`,
-              aspectRatio: `${game.board?.width}/${game.board?.height}`,
-            }}
-          >
-            {[1, board.height + 2].map((y) => {
-              return new Array(board.width).fill(0).map((_, i) => {
-                const x = i + 1;
-                return (
+            return (
+              <Box
+                key={i}
+                sx={{
+                  position: "relative",
+                  gridColumn: x + 2,
+                  gridRow: y + 2,
+                  aspectRatio: "1",
+                  backgroundColor: bgColor(),
+                  backgroundImage:
+                    agent && `url(${datas[agent.player].agentUrl})`,
+                  backgroundSize: "80%",
+                  backgroundPosition: "0% 50%",
+                  backgroundRepeat: "no-repeat",
+                  outline: "1px solid #555555",
+                  animation: isConflict ? `${flash} 1s linear infinite` : "",
+                  height: "100%",
+                  width: "100%",
+                }}
+                className="tile"
+              >
+                <Box
+                  sx={{
+                    position: "absolute",
+                    right: "0.2em",
+                    bottom: "0.2em",
+                  }}
+                >
+                  <BoardCellPoint isAbs={isAbs}>{point}</BoardCellPoint>
+                  {isAbs && <span>{Math.abs(point)}</span>}
+                </Box>
+                {agent && (
                   <Box
-                    key={`index-${x}-${y}`}
                     sx={{
-                      gridColumn: x + 1,
-                      gridRow: y,
+                      position: "absolute",
+                      top: "0.2em",
+                      left: "0.2em",
+                      width: "1em",
+                      height: "1em",
+                      borderRadius: "50%",
+                      backgroundColor: "yellow",
                       display: "flex",
-                      justifyContent: "center",
                       alignItems: "center",
-                      fontWeight: "bold",
-                    }}
-                  >
-                    {x}
-                  </Box>
-                );
-              });
-            })}
-            {[1, board.width + 2].map((x) => {
-              return new Array(board.width).fill(0).map((_, i) => {
-                const y = i + 1;
-                return (
-                  <Box
-                    key={`index-${x}-${y}`}
-                    sx={{
-                      gridColumn: x,
-                      gridRow: y + 1,
-                      display: "flex",
                       justifyContent: "center",
-                      alignItems: "center",
-                      fontWeight: "bold",
+                      fontSize: "0.8em",
+                      p: "0.5em",
                     }}
                   >
-                    {y}
+                    {agent.n + 1}
                   </Box>
-                );
-              });
-            })}
-            {(() => {
-              return tiled.map((tile, i) => {
-                const y = Math.floor(i / board.width);
-                const x = i % board.width;
-                const point = board.points[i];
-                const isAbs =
-                  point < 0 && tile.player !== null && tile.type === 0;
-                const agent = (() => {
-                  if (game.players) {
-                    const agent = game.players
-                      .map((e, i) =>
-                        e.agents.map((e_, j) => {
-                          return { agent: e_, player: i, n: j };
-                        })
-                      )
-                      .flat()
-                      .find((e) => e.agent.x === x && e.agent.y === y);
-                    return agent;
-                  } else return undefined;
-                })();
-                const bgColor = () => {
-                  if (tile.player !== null) {
-                    //console.log(tile);
-                    //console.log("tile player", tile.player);
-                    return datas[tile.player].colors[tile.type];
-                  } else if (point < 0) {
-                    const l = 100 - (Math.abs(point) * 50) / 16;
-                    return `hsl(0,0%,${l}%)`;
-                  } else if (point > 0) {
-                    const l = 100 - (Math.abs(point) * 50) / 16;
-                    return `hsl(60,100%,${l}%)`;
-                  }
-                };
-                const isConflict = game.log
-                  ? (() => {
-                      const lastActLog = game.log
-                        .at(-1)
-                        ?.players.flatMap((e) => {
-                          if (e.actions) return [...e.actions];
-                          else return [];
-                        });
-                      const isConflict = lastActLog?.some(
-                        (a) => a.res > 0 && a.res < 3 && a.x === x && a.y === y
-                      );
-                      return isConflict;
-                    })()
-                  : false;
-
-                return (
-                  <Box
-                    key={i}
-                    sx={{
-                      position: "relative",
-                      gridColumn: x + 2,
-                      gridRow: y + 2,
-                      aspectRatio: "1",
-                      backgroundColor: bgColor(),
-                      backgroundImage:
-                        agent && `url(${datas[agent.player].agentUrl})`,
-                      backgroundSize: "80%",
-                      backgroundPosition: "0% 50%",
-                      backgroundRepeat: "no-repeat",
-                      outline: "1px solid #555555",
-                      animation: isConflict
-                        ? `${flash} 1s linear infinite`
-                        : "",
-                      height: "100%",
-                      width: "100%",
-                    }}
-                    className="tile"
-                  >
-                    <Box
-                      sx={{
-                        position: "absolute",
-                        right: "0.2em",
-                        bottom: "0.2em",
-                      }}
-                    >
-                      <BoardCellPoint isAbs={isAbs}>{point}</BoardCellPoint>
-                      {isAbs && <span>{Math.abs(point)}</span>}
-                    </Box>
-                    {agent && (
-                      <Box
-                        sx={{
-                          position: "absolute",
-                          top: "0.2em",
-                          left: "0.2em",
-                          width: "1em",
-                          height: "1em",
-                          borderRadius: "50%",
-                          backgroundColor: "yellow",
-                          display: "flex",
-                          alignItems: "center",
-                          justifyContent: "center",
-                          fontSize: "0.8em",
-                          p: "0.5em",
+                )}
+                {agent &&
+                  (() => {
+                    const userId = players[agent.player].id;
+                    const user = users.get(userId);
+                    return (
+                      <AgentDetail
+                        style={{
+                          border: `solid 4px ${datas[agent.player].colors[1]}`,
+                          transform: getAgentTransform(x, y),
                         }}
                       >
-                        {agent.n + 1}
-                      </Box>
-                    )}
-                    {agent &&
-                      (() => {
-                        const userId = game.players[agent.player].id;
-                        const user = users.get(userId);
-                        return (
-                          <AgentDetail
-                            style={{
-                              border: `solid 4px ${
-                                datas[agent.player].colors[1]
-                              }`,
-                              transform: getAgentTransform(x, y),
-                            }}
-                          >
-                            <span>
-                              {user ? user.screenName : userId}
-                              {" : "}
-                              {agent.n + 1}
-                            </span>
-                            <br />
-                            <span>行動履歴</span>
-                            <AgentDetailHistory>
-                              {agentHistory(agent).map((e, i) => {
-                                return (
-                                  <div
-                                    key={i}
-                                    style={{
-                                      textDecoration:
-                                        e.res > 0 ? "line-through" : "none",
-                                    }}
-                                  >
-                                    T{e.turn}：
-                                    {e.type !== "停留" &&
-                                      `x:${e.x} , y:${e.y}に`}
-                                    {e.type}
-                                  </div>
-                                );
-                              })}
-                            </AgentDetailHistory>
-                          </AgentDetail>
-                        );
-                      })()}
-                  </Box>
-                );
-              });
-            })()}
-          </Box>
-        );
-      })()}
+                        <span>
+                          {user ? user.screenName : userId}
+                          {" : "}
+                          {agent.n + 1}
+                        </span>
+                        <br />
+                        <span>行動履歴</span>
+                        <AgentDetailHistory>
+                          {agentHistory(agent).map((e, i) => {
+                            return (
+                              <div
+                                key={i}
+                                style={{
+                                  textDecoration:
+                                    e.res > 0 ? "line-through" : "none",
+                                }}
+                              >
+                                T{e.turn}：
+                                {e.type !== "停留" && `x:${e.x} , y:${e.y}に`}
+                                {e.type}
+                              </div>
+                            );
+                          })}
+                        </AgentDetailHistory>
+                      </AgentDetail>
+                    );
+                  })()}
+              </Box>
+            );
+          });
+        })()}
+      </Box>
     </Box>
   );
 }
