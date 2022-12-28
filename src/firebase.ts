b4ec63516a1d4d5a264be529f32f198d29a53488
Suzu Tomo <kamekyame@outlook.com>
2022-12-28 17:25:26 +0900

    fix: 依存関係を総アップデート

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,27 +1,28 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/analytics";
-import "firebase/firestore";
+import { initializeApp, getApps, FirebaseApp } from "firebase/app";
+import { getAuth, connectAuthEmulator, Auth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
-if (typeof window !== "undefined" && firebase.apps.length === 0) {
-  const firebaseConfig = {
-    apiKey: "AIzaSyBOas3O1fmIrl51n7I_hC09YCG0EEe7tlc",
-    authDomain: "kakomimasu.firebaseapp.com",
-    projectId: "kakomimasu",
-    storageBucket: "kakomimasu.appspot.com",
-    messagingSenderId: "883142143351",
-    appId: "1:883142143351:web:dc6ddc1158aa54ada74572",
-    measurementId: "G-L43FT511YW",
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyBOas3O1fmIrl51n7I_hC09YCG0EEe7tlc",
+  authDomain: "kakomimasu.firebaseapp.com",
+  projectId: "kakomimasu",
+  storageBucket: "kakomimasu.appspot.com",
+  messagingSenderId: "883142143351",
+  appId: "1:883142143351:web:dc6ddc1158aa54ada74572",
+  measurementId: "G-L43FT511YW",
+};
 
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
-  firebase.analytics();
-  firebase.firestore();
+let app: FirebaseApp;
+let auth: Auth;
+if (typeof window !== undefined && getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
+  auth = getAuth();
 
   if (process.env.NEXT_PUBLIC_APISERVER_HOST?.includes("127.0.0.1")) {
-    auth.useEmulator(`http://127.0.0.1:9099`);
+    connectAuthEmulator(getAuth(app), `http://127.0.0.1:9099`);
+  } else {
+    getAnalytics(app);
   }
 }
 
-export default firebase;
+export { auth };
