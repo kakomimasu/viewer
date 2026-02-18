2c25e88fdabe44a5447fce32f88df842abc9543f
kamekyame <kamekyame@outlook.com>
2026-02-18 21:48:46 +0900

    👍 一部ドキュメントをNotionに移動

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,22 @@ const nextConfig: NextConfig = {
   experimental: {
     testProxy: true,
   },
+  redirects() {
+    return [
+      {
+        source: "/rule",
+        destination:
+          "https://kakomimasu.notion.site/2f693dd9b9fc8030baebe5c37d2beac8",
+        permanent: true,
+      },
+      {
+        source: "/docs/tutorial/docker-compose",
+        destination:
+          "https://kakomimasu.notion.site/docker-compose-30b93dd9b9fc80728e8ceb7000a1c8e1",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
